930f177d843188a25e944d4b39ce0efe1c9dfe10
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-22 23:19:36 -0500

    Updated licensing

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ use ezwin::prelude::*;
 fn main() -> WindowResult<()> {
   let window = Window::new(
     WindowSettings::default()
-      .with_close_on_x(false)
       .with_flow(Flow::Wait)
       .with_title("Easy Window")
       .with_size((800, 600)),
   )?;
 
   for msg in &window {
-    if let Message::CloseRequested = msg {
-      window.close();
-    }
-
     println!("{msg:?}")
   }
 
