88cdfba42118cee9aad49896e445339b56b5e1e7
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-24 18:26:27 -0500

    re-export raw_window_handle properly

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@
 #![cfg_attr(target_os, windows)] // for now, it only supports Win32
 #![deny(unsafe_op_in_unsafe_fn)]
 
+#[cfg(all(feature = "rwh_05", not(feature = "rwh_06")))]
+pub use rwh_05 as raw_window_handle;
+
+#[cfg(all(feature = "rwh_06", not(feature = "rwh_05")))]
+pub use rwh_06 as raw_window_handle;
+
 pub mod debug;
 mod handle;
 pub mod prelude;
