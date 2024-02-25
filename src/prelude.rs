fb23981458bc8ac9280d25b22da7b68d92796690
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-02-24 19:03:39 -0500

    Add to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,12 @@
 pub use crate::{
   debug::WindowResult,
   window::{
+    input::{
+      key::Key,
+      mouse::Button,
+      state::{ButtonState, KeyState},
+      Input,
+    },
     main_message::MainMessage,
     settings::{Flow, WindowSettings},
     window_message::{Message, MouseMessage, WindowMessage},
