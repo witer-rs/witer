87ea0379ed63b333a3cb0d89fc4fc37fb5743163
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-03-02 18:28:04 -0500

    Remove foxy

--- examples/foxy/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// #![feature(let_chains)]
-// #![feature(duration_constants)]
-#![deny(unsafe_op_in_unsafe_fn)]
-#![feature(associated_type_defaults)]
-
-pub mod core;
-pub mod prelude;
--- examples/foxy_renderer/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![deny(unsafe_op_in_unsafe_fn)]
-#![feature(associated_type_defaults)]
-#![allow(stable_features)]
-#![feature(c_str_literals)]
-#![warn(clippy::indexing_slicing)]
-
-pub mod error;
-pub mod renderer;
