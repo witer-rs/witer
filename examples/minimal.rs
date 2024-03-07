db7fd2e172d74a4c30acdf1d7f4725cde048b7f2
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-03-07 17:37:52 -0500

    Update minimal.rs

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -1,11 +1,6 @@
 use ezwin::prelude::*;
 
 fn main() {
-  let window = Window::new(WindowSettings::default()).unwrap();
-
-  for message in &window {
-    if let Message::Window(..) = message {
-      println!("{message:?}");
-    }
-  }
+  let window = Window::new(Default::default()).unwrap();
+  for _ in &window {}
 }
