427c1347c4edf39f1d81e39dc033b39af49186be
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-03-15 19:59:45 -0400

    Bump version

--- examples/threaded.rs
+++ examples/threaded.rs
@@ -88,7 +88,7 @@ fn app_loop(
         let message = message_receiver.try_recv().ok();
 
         if let Some(Message::MouseButton { .. } | Message::Key { .. }) = message {
-          info!("{:?}", window.current_monitor().scale_factor());
+          info!("{:?}", window.current_monitor().size());
         }
 
         match &message {
