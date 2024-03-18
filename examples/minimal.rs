4426287dd03d4d9d0b012ae3ef11a51b662c372b
Gabriel Lugo <g.alexander.lugo@gmail.com>
2024-03-18 00:21:05 -0400

    Re-export

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -2,8 +2,6 @@
   This example showcases the minimal amount to code required to open a window.
 */
 
-use witer::prelude::*;
-
 fn main() {
-  for _ in &Window::builder().build().unwrap() {}
+  for _ in &witer::Window::builder().build().unwrap() {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,32 @@ pub mod prelude;
 pub mod utilities;
 pub mod window;
 
+// re-exports
+pub use window::{
+  input::{
+    key::Key,
+    mouse::MouseButton,
+    state::{ButtonState, KeyState, RawKeyState},
+    Input,
+  },
+  message::{LoopMessage, Message, RawInputMessage},
+  settings::{WindowBuilder, WindowSettings},
+  state::{
+    CursorMode,
+    Flow,
+    Fullscreen,
+    LogicalPosition,
+    LogicalSize,
+    PhysicalPosition,
+    PhysicalSize,
+    Position,
+    Size,
+    Theme,
+    Visibility,
+  },
+  Window,
+};
+
 #[cfg(doctest)]
 #[doc = include_str!("../README.md")]
 struct ReadMe;
